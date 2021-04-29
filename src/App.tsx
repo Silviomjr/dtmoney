331607de40b6afe28e6178522af7e57794c95fe7
Silvio Manenti Junior <silviomjr@live.com>
2021-04-29 10:50:11 -0300

    class: 28/04/2021

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
 import { GlobalStyle } from "./styles/global";
 import { Header } from './components/header/index';
 import { Dashboard } from "./components/dashboard";
+import Modal from 'react-modal';
+import { useState } from "react";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 
+Modal.setAppElement('#root');
 
 export function App() {
+  const [isNewTransactionOpen, setIsNewTransactionOpen] = useState(false);
+    
+  function handleOpenNewTransactionModal() {
+      setIsNewTransactionOpen(true);
+  };
+  
+  function handleCloseNewTransactionModal() {
+      setIsNewTransactionOpen(false);
+  };
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+      <NewTransactionModal 
+        isOpen={isNewTransactionOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+
       <GlobalStyle />
     </>
   );
